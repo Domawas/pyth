5b1a73c78fd0214fd6c7af735071c5f77f711ad6
Nagy Dominik (Szf_N_2024_2026) <nagy.dominik@diak.szamalk-szalezi.hu>
2025-01-15 09:54:42 +0100

    pyth

---
+++ main.py
@@ -0,0 +1,6 @@
+import sorozat
+sorozat.general()
+sorozat.lista_kiir()
+nagyobb_db=sorozat.nagyobb(sorozat.lista)
+sorozat.konzol_ir(nagyobb_db)
+sorozat.fajlba_ir(nagyobb_db)
---
+++ sorozat.py
@@ -0,0 +1,33 @@
+import random
+lista=[]
+def general():
+    i=0
+    while i < 5:
+        lista.append(random.randint(1,10))
+        i+=1
+
+def lista_kiir():
+    i=0
+    while i <len(lista):
+        if i < len(lista)-1:
+            print(lista[i],end='!')
+        else:
+            print(lista[i],end='')
+        i+=1
+
+def nagyobb(lista):
+    i=0
+    nagyobb_db=0
+    while i < len(lista):
+        if lista[i]>lista[i-1]:
+            nagyobb_db+=1
+        i+=1
+    return nagyobb_db
+
+def konzol_ir(nagyobb):
+    print(f"\nNagyobb számok száma: {nagyobb}")
+
+
+def fajlba_ir(nagyobb_db):
+    with open("vegeredmeny.txt","w",encoding="UTF-8") as file:
+        file.write(f"Nagyobb számok száma: {nagyobb_db}")
